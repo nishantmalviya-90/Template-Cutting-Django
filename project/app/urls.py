4154e5944510f002fef45c2d2c0c7d1e41df2a0f
Nishant Malviya <164031499+nishantmalviya-90@users.noreply.github.com>
2024-08-21 21:14:57 +0530

    Template cutting

---
+++ project/app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import home,contact,about
+urlpatterns=[
+    path("",home,name="home"),
+    path("about/",about,name="about"),
+    path("contact/",contact,name="contact"),
+]
